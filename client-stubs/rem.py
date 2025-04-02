b641be690f4e95734520a0d6ff5e3120e55df96d
christine-george <christineg@rewiringamerica.org>
2025-04-02 12:21:29 -0400

    your

--- client-stubs/rem.py
+++ client-stubs/rem.py
@@ -4,7 +4,7 @@ import rewiringamerica_rem
 # Set up your key to send authorized requests to the API. If you
 # don't already have one, you can sign up for one here: https://rewiring.link/api-signup
 #
-# Please do not commit you API key to any public repository. There is an
+# Please do not commit your API key to any public repository. There is an
 # additional example in this repository that shows how to secure your API key
 # using Next.js. It is in the `rem-with-nextjs/` directory.
 configuration = rewiringamerica_rem.Configuration(
--- client-stubs/rem.ts
+++ client-stubs/rem.ts
@@ -7,7 +7,7 @@ import { HttpBearerAuth } from "@rewiringamerica/rem/model/models";
 // Set up your key to send authorized requests to the API. If you don't already have
 // one, you can sign up for one here: https://rewiring.link/api-signup
 //
-// Please do not commit you API key to any public repository. There is an additional
+// Please do not commit your API key to any public repository. There is an additional
 // example in this repository that shows how to secure your API key using Next.js.
 // It is in the `rem-with-nextjs/` directory.
 const key = new HttpBearerAuth();
